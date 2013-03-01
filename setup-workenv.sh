47330f4e77dce21fa55c59da957235a4ff34a21c
Axel Philipsenburg <axel@wired-space.de>
2013-03-01 11:22:28 +0100

    - Code format changed: If and For statements. - Allow for non interactive run by presetting git infos. - Added functionality to install gnome-shell

--- setup-workenv.sh
+++ setup-workenv.sh
@@ -6,6 +6,13 @@ set -u
 ## Set some variables
 SKYPE_DOWNLOAD_URL="http://www.skype.com/go/getskype-linux-beta-ubuntu-64"
 
+## For non-interactive setup set these ...
+# GITUSERNAME=""
+# GITUSEREMAIL=""
+
+## Set to 0 if you don't want to install the Gnome-Shell desktop environment
+INSTALL_GNOME_SHELL=1
+
 ## Start working
 echo '* Updating to latest versions ...'
 sudo apt-get -y update  > /dev/null
@@ -17,16 +24,14 @@ sudo apt-get install -y build-essential > /dev/null
 echo '  ... done.'
 
 echo '* Installing tools to fetch software and stuff ...'
-for tool in aptitude git-core subversion subversion-tools wget curl mercurial rsync lftp s3cmd;
-do
+for tool in aptitude git-core subversion subversion-tools wget curl mercurial rsync lftp s3cmd; do
 	echo "  - ${tool}"
 	sudo apt-get -y install ${tool} > /dev/null
 done
 echo '  ... done.'
 
 echo '* Adding PPAs ...'
-for ppa in ppa:tiheum/equinox;
-do
+for ppa in ppa:tiheum/equinox ppa:gnome3-team/gnome3 ppa:webupd8team/themes; do
 	echo "  - ${ppa}"
 	sudo apt-add-repository -y ${ppa} > /dev/null
 done
@@ -40,16 +45,14 @@ echo '  ... done.'
 
 
 echo '* Installing system information tools ...'
-for tool in htop bmon iftop iotop dstat;
-do
+for tool in htop bmon iftop iotop dstat; do
 	echo "  - ${tool}"
 	sudo apt-get -y install ${tool} > /dev/null
 done
 echo '  ... done.'
 
 echo '* Installing basic system tools ...'
-for tool in ack;
-do
+for tool in ack; do
 	echo "  - ${tool}"
 	sudo apt-get -y install ${tool} > /dev/null
 done
@@ -57,8 +60,7 @@ echo '  ... done.'
 
 
 echo '* Installing tools for terminal usage ...'
-for tool in mc pv unp rar p7zip-full pbzip2 screen tmux terminator;
-do
+for tool in mc pv unp rar p7zip-full pbzip2 screen tmux terminator; do
 	echo "  - ${tool}"
 	sudo apt-get -y install ${tool} > /dev/null
 done
@@ -73,8 +75,7 @@ sudo apt-get -y install emacs24-nox > /dev/null
 echo '  ... done.'
 
 echo '* Installing communication tools ...'
-for tool in thunderbird thunderbird-lightning thunderbird-enigmail pidgin pidgin-otr xchat xchat-otr;
-do
+for tool in thunderbird thunderbird-lightning thunderbird-enigmail pidgin pidgin-otr xchat xchat-otr; do
 	echo "  - ${tool}"
 	sudo apt-get -y install ${tool} > /dev/null
 done
@@ -85,7 +86,7 @@ wget -O ./skype.deb ${SKYPE_DOWNLOAD_URL}  > /dev/null
 ## apt-get command will fix that, but we need to disable "-e" 
 ## for it temporarily.
 set +e
-sudo dpkg -i ./skype.deb > /dev/null
+sudo dpkg -i ./skype.deb &> /dev/null
 set -e
 
 sudo apt-get -y -f install > /dev/null
@@ -93,18 +94,26 @@ popd
 
 echo '  ... done.'
 
-
 echo '* Installing developer tools ...'
-for tool in kdiff3 wireshark htop dstat bmon iftop;
-do
+for tool in kdiff3 wireshark htop dstat bmon iftop; do
 	echo "  - ${tool}"
 	sudo apt-get -y install ${tool} > /dev/null
 done
 echo '  ... done.'
 
+if [[ ${INSTALL_GNOME_SHELL} = "1" ]]; then
+
+    echo '* Installing gnome shell desktop environment ...'
+    sudo DEBIAN_FRONTNED=noninteractive apt-get -y ubuntu-gnome-desktop ubuntu-gnome-default-settings gnome-documents > /dev/null
+    sudo apt-get -y remove ubuntu-settings > /dev/null
+    sudo update-rc.d lightdm remove
+    sudo update-rc.d gdm defaults
+    echo '  ... done.'
+
+fi
+
 echo '* Installing look and feel packages ...'
-for tool in faience-theme faience-icon-theme;
-do
+for tool in faience-theme faience-icon-theme mediterraneannight-gtk-theme; do
 	echo "  - ${tool}"
 	sudo apt-get -y install ${tool} > /dev/null
 done
@@ -112,13 +121,11 @@ echo '  ... done.'
 
 echo '* Preparing basic SSH dir for user '${USER} ...
 pushd $HOME > /dev/null
-if [ ! -d .ssh ]
-then
+if [ ! -d .ssh ]; then
     mkdir .ssh > /dev/null
     chmod 700 .ssh > /dev/null
 fi
-if [ ! -f .ssh/config ]
-then
+if [ ! -f .ssh/config ]; then
     cat > .ssh/config <<EOF
 ## SSH client config
 
@@ -146,11 +153,15 @@ fi
 popd > /dev/null
 
 echo '* Doing basic git configuration for user '${USER} ...
-read -p ' git full user name (for message): ' GITUSERNAME
+if [[ -z "$GITUSERNAME" ]]; then
+    read -p ' git full user name (for message): ' GITUSERNAME
+fi
 git config --global user.name "${GITUSERNAME}"
-read -p ' git user email (for message): ' GITUSEREMAIL
+if [[ -z "$GITUSEREMAIL" ]]; then
+    read -p ' git user email (for message): ' GITUSEREMAIL
+fi
 git config --global user.email "${GITUSEREMAIL}"
-git config --global core.editor 'emacs24-nox'
+git config --global core.editor 'emacs'
 git config --global color.ui true
 KDIFF3PATH=$(which kdiff3)
 cat >> ${HOME}/.gitconfig <<EOF
@@ -182,8 +193,7 @@ echo '* Doing some settings ...'
 echo '  - Disabling overlay scollbars'
 echo "export LIBOVERLAY_SCROLLBAR=0" >> ${HOME}/.xprofile
 echo '  - Setting up my bash extra settings'
-if [ ! -f ${HOME}/.bash_extras ]
-then
+if [ ! -f ${HOME}/.bash_extras ]; then
 cat > ${HOME}/.bash_extras <<EOF
 # We know that terminator supports this
 export TERM=xterm-256color
